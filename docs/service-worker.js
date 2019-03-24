4a7f90cb38dea9a3627ff0962c9ac072de96fa03
Biagio Azzarelli <b.azzarelli@gmail.com>
2019-03-23 17:30:51 -0700

    rebuild the static site

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2748bb590bcfb0db6be1c849d4ab990f.js"
+  "/i-ching/precache-manifest.b7fb1feafe24de64dacbf054db8954d8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/i-ching/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
