dcb9dfe54385d1450115ee5390666ed373cdea04
Biagio Azzarelli <b.azzarelli@gmail.com>
2019-08-02 13:33:44 -0700

    adding tutorial link

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
-
+// tutorial:
+// https://medium.com/@notrab/getting-started-with-create-react-app-redux-react-router-redux-thunk-d6a19259f71f
 export default combineReducers({
     counter,
 });
